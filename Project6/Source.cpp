d1234be7befb942da29389edb89736c853734004
Djhaver <avidjhaveri@gmail.com>
2014-04-03 20:47:41 -0700

    Update Source.cpp

--- Project6/Source.cpp
+++ Project6/Source.cpp
@@ -2,175 +2,31 @@
 #include <fstream>
 #include <string>
 #include "Date_Time.h"
+#include "FlightNode.h"
+#include "HubNode.h"
+#include "search.h"
 
 using namespace std;
 
 class FlightNode;
 class HubNode;
 class Date_Time;
-class FlightPlan;
-
-HubNode *head = NULL, *hubTemp;
-FlightNode *flightTemp;
-
-class HubNode {
-public:	
-	string Name;
-	string Location;
-	HubNode* next;
-	FlightNode* headFlights;
-
-	HubNode::HubNode()
-	{
-	}
-	
-	virtual ~HubNode()
-	{
-		do
-		{
-			hubTemp = head->next;
-			delete head;
-			head = hubTemp;
-		}while (head != NULL);
-
-	}
-};
 
 HubNode* hubSearch (string);
-
-class FlightNode {
-public:
-	string flightNumber;
-	double price;
-	string flightCompany;
-	Date_Time departure;
-	int duration;
-	HubNode* source;
-	HubNode* destination;
-	FlightNode * next;
-
-	FlightNode::FlightNode()
-	{
-	}
-
-	int getBaggageFees();
-	int getDelay();
-
-	/*virtual ~FlightNode()
-	{
-		do
-		{
-			flightTemp = flightHead->next;
-			delete flightHead;
-			flightHead = flightTemp;
-		}while (flightHead != NULL);
-	}*/
-};
-
-class FlightPlan
-{
-
-};
-
-class Southwest: public FlightNode
-{
-public:
-	int bags;
-
-	Southwest(): FlightNode()
-	{
-	}
-
-	void setBags(int Bags)
-	{
-		bags = Bags;
-	}
-
-	int getBaggageFees()
-	{
-		int fees;
-		
-		fees = bags * 25;
-		return fees;
-	}
-
-	int getDelay()
-	{
-		if (departure.hours >= 7 || departure.hours <= 17)
-			return 30;
-		else
-			return 0;
-	}
-};
-
-class Delta: public FlightNode
-{
-public:
-	int bags;
-
-	Delta(): FlightNode()
-	{
-	}
-
-	void setBags(int Bags)
-	{
-		bags = Bags;
-	}
-
-	int getBaggageFees()
-	{
-		return 0;
-	}
-
-	int getDelay()
-	{
-		return 20;
-	}
-};
-
-class USAirways: public FlightNode
-{
-public:
-	int bags;
-	
-	USAirways(): FlightNode()
-	{
-	}
-
-	void setBags(int Bags)
-	{
-		bags = Bags;
-	}
-
-	int getBaggageFees()
-	{
-		int fees;
-		
-		if (bags > 0)
-			fees = (bags-1) * 25;
-		else 
-			return 0;
-
-		return fees;
-	}
-
-	int getDelay()
-	{
-		if (departure.hours >= 7 || departure.hours <= 17)
-			return 10;
-		else if (departure.hours > 17 || departure.hours <= 1)
-			return 15;
-		else
-			return 0;
-	}
-};
 
 void printHub()
 {
 	hubTemp = head;
+	
 	while (hubTemp != NULL)
 	{
-		cout << hubTemp -> Name << ' ' << hubTemp -> Location << '\n';
+		FlightNode *flight = hubTemp -> headFlights;
+		cout << hubTemp -> Name << ' ' << hubTemp -> Location << endl;
+		while (flight != NULL)
+		{
+			cout << flight -> flightNumber << endl;
+			flight = flight -> next;
+		}
 		hubTemp = hubTemp -> next;
 	}
 }
@@ -206,8 +62,6 @@ int main () {
 	}
 	else cout << "Unable to open file"; 
 
-	printHub();
-
 	string passline, number, price, source, destination, departuremin,departurehour,
 		departureday,departuremonth,departureyear,duration, company;
 	int min, hour, day, month, year;
@@ -228,7 +82,7 @@ int main () {
 			getline(flightfile, duration, ',');
 			getline(flightfile, company);
 
-			if (company.compare("Southwest") == 0)
+			if (company.compare("SouthWest") == 0)
 				flightTemp = new Southwest();
 			else if (company.compare("Delta") == 0)
 				flightTemp = new Delta();
@@ -244,18 +98,24 @@ int main () {
 			year = atoi(departureyear.c_str());
 			flightTemp -> duration = atoi(duration.c_str());
 
-			Date_Time y = Date_Time(min, hour, day, month, year);
+			Date_Time depart = Date_Time(min, hour, day, month, year);
+			Date_Time arrive = depart;
+			arrive.AddMinutes((flightTemp->duration + flightTemp->getDelay()));
 			
-			flightTemp -> departure = y;
-			flightTemp -> source = hubSearch(source);
+			flightTemp -> departure = &depart;
+			hubTemp = hubSearch(source);
+			flightTemp -> source = hubTemp;
 			flightTemp -> destination = hubSearch(destination);
 			flightTemp -> next = flightTemp -> source -> headFlights;
-			flightTemp -> source -> headFlights = flightTemp;
+			hubTemp -> headFlights = flightTemp;
 	    }
 		myfile.close();
 	}
 	else cout << "Unable to open file";
-	
+	Date_Time start = Date_Time(00,0, 16, 12,2013);
+	Date_Time end = Date_Time(59,23,18,12,2013);
+	flightSearch(start, end, "Los Angeles", 3, "Cheapest");
+	printHub();
 	cout << '\n';
 	freeHub();
 	cin >> x;
@@ -269,7 +129,7 @@ HubNode* hubSearch(string search)
 	{
 		if (search.compare(p -> Name) == 0)
 		{
-			return b;
+			return p;
 		}
 		else
 		{
